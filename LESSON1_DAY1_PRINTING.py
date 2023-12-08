59ec1bfde35419aac58b9b1a62cb3c3baac3746e
VED PRAKASH <153211377+VED-PRAKA5H@users.noreply.github.com>
2023-12-08 21:24:00 +0530

    LESSON1_DAY1_PRINTING.py

---
+++ LESSON1_DAY1_PRINTING.py
@@ -0,0 +1,15 @@
+#Q
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+#OUTPUT
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+
+#ANSWER
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
