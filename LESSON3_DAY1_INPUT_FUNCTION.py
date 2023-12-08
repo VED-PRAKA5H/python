d4a475e766fbd1c5d7d593a79e167b276bef85d9
VED PRAKASH <153211377+VED-PRAKA5H@users.noreply.github.com>
2023-12-08 22:12:13 +0530

    LESSON3_DAY1_INPUT_FUNCTION.py
    INPUT AND LENGTH FUNCTION

---
+++ LESSON3_DAY1_INPUT_FUNCTION.py
@@ -0,0 +1,11 @@
+num1 = int(input('what is the first no?\n'))
+num2 = int(input('What is the second number?\n'))
+print(num1*num2)
+
+
+
+# Provide any name in the input pane below.
+# That value can be accessed using the input() function.
+# Don't put anything inside the input() function!
+# Write your code below this line 👇
+print(len(input("Input a word to find no. letters\n")))
