d05e44fa0a3938f8b798299e153240e0a7376654
VED PRAKASH <153211377+VED-PRAKA5H@users.noreply.github.com>
2023-12-10 13:29:24 +0530

    LESSON14_DAY4_BANKER_ROULETTE.py
    store the names in a list by comma delimiter, you can use the split() method in Python.

--- LESSON14_DAY4_BANKER_ROULETTE.py
+++ LESSON14_DAY4_BANKER_ROULETTE.py
@@ -1,11 +1,11 @@
-print("Enter names: ")
-names_string= input()
-names = names_string.split(", ")
+names_string=input("Enter names in one sentence whith comma\n")
+names = names_string.split(",")
 # The code above converts the input into an array seperating
 #each name in the input by a comma and space.
 # 🚨 Don't change the code above 👆
 length=len(names)-1
 import random
 x = random.randint(0,length)
+print(x)
 random_name= names[x]
 print(f"{random_name} is going to buy the meal today!")
